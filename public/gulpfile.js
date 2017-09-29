7c507448a113b426fefce6a2e2beae6177f62c53
mark342703702 <342703702@qq.com>
2017-09-29 17:36:53 +0800

    asdasdss

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,1 +1,23 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer  = require('autoprefixer');//添加私有属性前缀
+
+//编译scss文件为css文件并添加私有属性前缀
+gulp.task('sass', function(){
+    var processors = [
+        autoprefixer
+    ];
+
+    return gulp.src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('css'))
+
+});
+
+gulp.task('default', ['sass'], function(event){
+
+    gulp.watch(['sass/**/*.scss'], ['sass']);
+
+});
