462d02e5aec3447964cc95b5d4f1d2a27b5f1e08
mark342703702 <342703702@qq.com>
2017-09-30 17:58:35 +0800

    add chart

---
+++ public/js/home.js
@@ -0,0 +1,16 @@
+$(function(){
+    var data = {
+	labels : ["January","February","March","April","May","June","July"],
+	datasets : [
+		{
+			fillColor : "rgba(151,187,205,0.5)",
+			strokeColor : "rgba(151,187,205,1)",
+			data : [28,48,40,19,96,27,100]
+		}
+	]
+}
+
+var ctx = $("#myChart").get(0).getContext("2d");
+var myNewChart = new Chart(ctx).Bar(data);
+
+});
