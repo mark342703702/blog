ab55f985a53ef9a7cbb62e847ba8fdcdbcf5eaf6
mark342703702 <342703702@qq.com>
2017-09-30 16:23:28 +0800

    add layout and login

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,9 @@
 $(document).ready(function() {
   $('#particles').particleground({
     dotColor: '#24d8e3',
-    lineColor: '#fff'
+    lineColor: '#fff',
+    minSpeedX: 0.3,
+    minSpeedY: 0.2
   });
-  
+
 });
